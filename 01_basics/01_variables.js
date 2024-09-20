329e5336448a8a508bac89b26da9136eeac8e9b4
Priyam-77818 <161926678+Priyam-77818@users.noreply.github.com>
2024-09-20 14:23:50 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail="priyam8@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+
+
+// accountId=2//not allowed
+
+accountEmail="hdg8@gmail.com"
+accountPassword="672832792"
+accountCity="bengluru"
+
+// console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";//treat all js code as newer version
+
+//alert("hello")// we are using nodejs not browser
+
+console.log(3+3);
+console.log("priyam");//code readeability it should be high
+let name="priyam"
+let age=19
+let isLoggedIn=false
+
+//primitive data types
+//number=>2 to power53
+//bigint for bigger numbers
+//string=> ""
+//boolean=> true or false
+//null=>standalone value
+//undefined=>value not assigned yet
+//symbol=>to find uniqueness
+
+//objects
+
+console.log(typeof null)//object type
+console.log(typeof undefined)//undefined type
